831840689f2d9944808b6b1d312779795b630699
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2025-08-22 09:26:19 -0700

    Add circumference

--- circle.py
+++ circle.py
@@ -1,4 +1,8 @@
+PI = 3.14159265358979323846264338327950
+
 class Circle:
     def __init__(self, radius):
         self.radius = radius
     
+    def circumference(self):
+        return 2 * self.radius * PI
