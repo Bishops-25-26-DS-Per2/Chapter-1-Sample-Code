7d46b1f55353bf8f1e828c899d018e31179e8375
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2025-08-25 14:26:18 -0700

    Add annotations and docstring.

--- circle.py
+++ circle.py
@@ -1,8 +1,9 @@
 PI = 3.14159265358979323846264338327950
 
 class Circle:
-    def __init__(self, radius):
+    def __init__(self, radius: float):
         self.radius = radius
     
-    def circumference(self):
+    def circumference(self) -> float:
+        """Calculate the circumference based on the radius attribute."""
         return 2 * self.radius * PI
