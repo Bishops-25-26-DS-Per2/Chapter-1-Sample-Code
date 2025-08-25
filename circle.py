11758bf36533ceeccce4f8e9ae9892d532b54b82
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2025-08-25 14:52:24 -0700

    Create an instance, add __str__, view help(circle).

--- circle.py
+++ circle.py
@@ -4,6 +4,17 @@ class Circle:
     def __init__(self, radius: float):
         self.radius = radius
     
+    def __str__(self) -> str:
+        return f"Circle object with radius {self.radius}"
+
     def circumference(self) -> float:
         """Calculate the circumference based on the radius attribute."""
         return 2 * self.radius * PI
+
+def main():
+    circle = Circle(3)
+    print(circle)
+    help(circle)
+
+if __name__ == "__main__":
+    main()
